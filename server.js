a8c9851726a1098e42ad8f0464e931ca57a2d8ec
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-08 20:24:27 +0530

    upadateSomePackeges

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-const express = requair("express")
+const express = require("express")
+const { execPath } = require("process")
 const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log("server is running")
+})
