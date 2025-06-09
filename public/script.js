51023380e9c7d8235c7fe82990870e72d9a94631
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-09 23:14:15 +0530

    finish the app

---
+++ public/script.js
@@ -0,0 +1,54 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("joinForm");
+    if (form) {
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+            const username = document.getElementById("UserName").value.trim();
+            if (username) {
+                localStorage.setItem("username", username);
+                window.location.href = "chat.html";
+            }
+        });
+    }
+});
+
+const socket = io()
+
+const chatBox = document.getElementById('chatBox')
+const chatForm = document.getElementById('chatForm')
+const messageInput = document.getElementById('messageInput')
+
+const username = localStorage.getItem("username") || "Anonymous"
+socket.emit('newUser',username)
+
+chatForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    const message=messageInput.value.trim()
+    if(message){
+        renderMessage('You',message)
+        socket.emit('chat',{username,text:message})
+        messageInput.value=""
+    }
+})
+
+socket.on('chat',(data)=>{
+    renderMessage(data.username,data.text)
+})
+
+socket.on('message',(msg)=>{
+    const div = document.createElement('div')
+    div.textContent=msg
+    div.className="text-gray-300 italic"
+    chatBox.appendChild(div)
+    chatBox.scrollTop=chatBox.scrollHeight
+})
+
+function renderMessage(sender,message){
+    const div = document.createElement("div");
+      div.innerHTML = `<strong>${sender}:</strong> ${message}`;
+      chatBox.appendChild(div);
+      chatBox.scrollTop = chatBox.scrollHeight;
+}
+
--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require("express")
-const { execPath } = require("process")
-const app = express()
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 
+
+io.on('connection',(socket)=>{
+    console.log("new user connected")
+
+    socket.on("newUser",(username)=>{
+        socket.username = username
+        socket.broadcast.emit("message",`${username} joined the chat`)
+    })
+    socket.on("chat",(message)=>{
+      socket.broadcast.emit("chat",message)
+    })
+    socket.on("disconnect",()=>{
+      if(socket.username){
+        socket.broadcast.emit("message",`${socket.username} left the chat`)
+      }
+    })
+})
+
 const PORT = 3000
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log("server is running")
 })
