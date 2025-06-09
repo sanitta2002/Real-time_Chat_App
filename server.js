4725ead6a1e2042fbac9499e841970a98c038fb8
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-09 23:26:29 +0530

    finish

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
-const express = require("express")
-const path = require('path')
-const http = require('http')
-const socketio = require('socket.io')
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
+app.use(express.static(path.join(__dirname, "public")));
 
+io.on("connection", (socket) => {
+  console.log("new user connected");
 
-app.use(express.static(path.join(__dirname, 'public')))
+  socket.on("newUser", (username) => {
+    socket.username = username;
+    socket.broadcast.emit("message", `${username} joined the chat`);
+  });
+  socket.on("chat", (message) => {
+    socket.broadcast.emit("chat", message);
+  });
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      socket.broadcast.emit("message", `${socket.username} left the chat`);
+    }
+  });
+});
 
-
-
-io.on('connection',(socket)=>{
-    console.log("new user connected")
-
-    socket.on("newUser",(username)=>{
-        socket.username = username
-        socket.broadcast.emit("message",`${username} joined the chat`)
-    })
-    socket.on("chat",(message)=>{
-      socket.broadcast.emit("chat",message)
-    })
-    socket.on("disconnect",()=>{
-      if(socket.username){
-        socket.broadcast.emit("message",`${socket.username} left the chat`)
-      }
-    })
-})
-
-const PORT = 3000
-server.listen(PORT,()=>{
-    console.log("server is running")
-})
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log("server is running");
+});
